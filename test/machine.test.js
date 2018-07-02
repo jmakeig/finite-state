a73200489d399a8659b07976499e240e9af30ec5
Justin Makeig <jmpublic+github@makeig.com>
2018-07-02 15:13:06 -0700

    Kinda sorta working before refactor

--- test/machine.test.js
+++ test/machine.test.js
@@ -13,23 +13,34 @@ function App(machine) {
     }
     return state;
   }
-
-  this.store = createStore(reducer, {});
-
-  this.store.subscribe(() => {
+  
+  this._store = createStore(reducer, {});
+  this._store.subscribe(() => {
     this.dispatch(this.store.getState());
   });
 }
+Object.defineProperties(App.prototype, {
+  state: {
+    enumerable: true,
+    writable: false,
+    getter: () => this.store.getState()
+  }
+});
 App.prototype.dispatch = function(state) {
   //
 };
-App.prototype.transition = function(event) {
+App.prototype.transition = function(event, action) {
   console.log(`Transitioning from ${this.currentState.value} using ${event}`);
   const curr = this._machine.transition(this.currentState, event);
+  if (action && action.type) {
+    console.log(`Updating state with ${action.type}`);
+    this._store.dispatch(action);
+  }
   this.currentState = curr;
   if (curr.actions && curr.actions.length) {
     return Promise.all(curr.actions.map(action => this[action]()));
   }
+  // TODO: This looks weird
   return new Promise(resolve => resolve());
 };
 
@@ -37,7 +48,12 @@ App.prototype.transition = function(event) {
 function doLoadMarkdown() {
   // QUESTION: How do I fake a delay here with setTimeout?
   // return new Promise(resolve => setTimeout(() => resolve('myFile.md'), 200));
-  return new Promise(resolve => resolve('myFile.md'));
+  const payload = {
+    file: 'some-file.md',
+    markdown: '# Hello, world!\n\nIpsum lorem…'
+  };
+  // TODO: Load and parse Markdown file from the file system
+  return new Promise(resolve => resolve(payload));
 }
 
 // Instance method matches `onEntry` state action
@@ -45,18 +61,18 @@ App.prototype.loadMarkdown = function() {
   return new Promise((resolve, reject) => {
     doLoadMarkdown()
       .then(file => {
-        console.log(file);
-        this.store.dispatch({ type: 'LOAD_MARKDOWN', payload: { file } });
-        this.transition('success');
+        this.transition('success', {
+          type: 'LOAD_MARKDOWN',
+          payload: { file }
+        });
         resolve();
       })
       .catch(error => {
-        this.store.dispatch({
+        this.transition('error', {
           type: 'LOAD_MARKDOWN_ERROR',
           payload: { error }
         });
-        this.transition('error');
-        reject();
+        reject(error);
       });
   });
 };
