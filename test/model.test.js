a41b483c2b8f3d7fe8f1c3eef1ba3b919405f08a
Justin Makeig <jmpublic+github@makeig.com>
2018-07-19 13:25:45 -0700

    Adds JSON Schema tests

---
+++ test/model.test.js
@@ -0,0 +1,22 @@
+import { matchers } from 'jest-json-schema';
+import * as schema from '../model.schema.json';
+
+expect.extend(matchers);
+
+test('Valid schema', () => {
+  expect(schema).toBeValidSchema();
+});
+
+test('Dummy instance', () => {
+  expect(schema).toBeValidSchema();
+  const model = {
+    ui: { user: null, currentSelection: null, activeAnnotationID: null },
+    model: {
+      href: null,
+      mime: null,
+      content: null,
+      annotations: []
+    }
+  };
+  expect(model).toMatchSchema(schema);
+});
